1b4076142046565ab612bbc380e6520a489dd400
returaj <returaj.burnwal@gmail.com>
2023-10-05 23:45:44 +0530

    add code

--- gan/critic_trainer.py
+++ gan/critic_trainer.py
@@ -9,58 +9,59 @@ import optax
 from gan_mpc import utils
 
 
+def split_dataset(dataset, key):
+    X, Y = dataset
+    datasize = Y.shape[0]
+    halfsize = datasize // 2
+    perm = jax.random.choice(key, datasize, shape=(2, halfsize))
+    true_data = X[perm[0]], Y[perm[0]]
+    pred_data = X[perm[1]], Y[perm[1]]
+    return (true_data, pred_data)
+
+
 @functools.partial(jax.jit, static_argnums=0)
-def get_dataset(policy, params, true_dataset, key):
-    @jax.jit
-    def func(X, true_Y):
-        datasize = true_Y.shape[0]
-        true_label = jnp.ones(datasize, dtype=jnp.float32)
-        xsize = X.shape[-1]
+def get_pred_dataset(policy, params, pred_X):
+    xsize = pred_X.shape[-1]
 
-        def predict(params, x):
-            xc, *_ = policy.get_optimal_values(params, x)
-            y, _ = jnp.split(xc, [xsize], axis=-1)
-            return y
+    def predict(x):
+        xc, *_ = policy.get_optimal_values(params, x)
+        y, _ = jnp.split(xc, [xsize], axis=-1)
+        return y
 
-        pred_Y = jax.vmap(predict, in_axes=(None, 0))(params, X)
-        pred_label = jnp.zeros(datasize, dtype=jnp.float32)
-
-        return (
-            jnp.concatenate([true_Y, pred_Y], axis=0),
-            jnp.concatenate([true_label, pred_label], axis=0),
-        )
-
-    true_train_data, true_test_data = true_dataset
-    train_X, train_label = func(*true_train_data)
-    test_X, test_label = func(*true_test_data)
-
-    perm = jax.random.permutation(key, train_X.shape[0])
-    return (train_X[perm], train_label[perm]), (test_X, test_label)
+    pred_Y = jax.vmap(predict)(pred_X)
+    return pred_Y
 
 
 @functools.partial(jax.jit, static_argnums=0)
 def calculate_loss(policy, params, dataset):
-    X, Y = dataset
-    losses = jax.vmap(policy.critic_loss, in_axes=(0, 0, None))(X, Y, params)
+    (_, true_y), (pred_x, _) = dataset
+    pred_y = get_pred_dataset(policy, params, pred_x)
+    losses = jax.vmap(policy.critic_loss, in_axes=(0, 0, None))(
+        true_y, pred_y, params
+    )
     return jnp.mean(losses)
 
 
 @functools.partial(jax.jit, static_argnums=0)
-def train_critic_parameters(train_args, opt_state, params, perm, dataset):
+def train_critic_parameters(train_args, opt_state, params, perm, dataset, key):
     policy, opt = train_args
-    X, Y = dataset
+    (_, true_Y), (pred_X, _) = dataset
 
     @jax.jit
     def body(carry, p):
-        params, opt_state = carry
-        batch_x, batch_y = X[p], Y[p]
-        loss, grads = policy.critic_loss_and_grad(batch_x, batch_y, params)
+        params, opt_state, key = carry
+        key, subkey = jax.random.split(key)
+        batch_true_y, batch_pred_x = true_Y[p], pred_X[p]
+        batch_pred_y = get_pred_dataset(policy, params, batch_pred_x)
+        loss, grads = policy.critic_loss_and_grad(
+            batch_true_y, batch_pred_y, params, subkey
+        )
         updates, opt_state = opt.update(grads, opt_state, params)
         params = optax.apply_updates(params, updates)
-        return (params, opt_state), loss
+        return (params, opt_state, key), loss
 
-    (params, opt_state), batch_loss = jax.lax.scan(
-        body, (params, opt_state), perm
+    (params, opt_state, _), batch_loss = jax.lax.scan(
+        body, (params, opt_state, key), perm
     )
     return params, opt_state, jnp.mean(batch_loss)
 
@@ -79,14 +80,16 @@ def train(
     del id
     policy, opt = train_args
     key, subkey = jax.random.split(key)
-    train_data, test_data = get_dataset(policy, params, true_dataset, subkey)
-    datasize = train_data[0].shape[0]
+    true_train_data, true_test_data = true_dataset
+    train_data = split_dataset(true_train_data, subkey)
+    test_data = split_dataset(true_test_data, subkey)
+    datasize = train_data[0][0].shape[0]
     steps_per_update = datasize // batch_size
     train_losses, test_losses = [], []
     for _ in range(1, num_updates + 1):
-        key, subkey = jax.random.split(key)
+        key, subkey1, subkey2 = jax.random.split(key, 3)
         perm = jax.random.choice(
-            subkey, datasize, shape=(steps_per_update, batch_size)
+            subkey1, datasize, shape=(steps_per_update, batch_size)
         )
         params, opt_state, train_loss = train_critic_parameters(
             train_args=(policy, opt),
@@ -94,6 +97,7 @@ def train(
             params=params,
             perm=perm,
             dataset=train_data,
+            key=subkey2,
         )
         test_loss = calculate_loss(
             policy=policy, params=params, dataset=test_data
--- gan/js_policy.py
+++ gan/js_policy.py
@@ -24,6 +24,7 @@ class JS_MPC(base.BaseMPC):
         critic_model,
         loss_vmap=(0,),
         entropy_coef=1e-3,
+        grad_penalty_coef=10.0,
         trajax_ilqr_kwargs=eval.TRAJAX_iLQR_KWARGS,
     ):
         super().__init__(
@@ -35,6 +36,7 @@ class JS_MPC(base.BaseMPC):
             trajax_ilqr_kwargs,
         )
         self.entropy_coef = entropy_coef
+        self.grad_penalty_coef = grad_penalty_coef
         self.critic_model = critic_model
 
     def init(
@@ -46,21 +48,52 @@ class JS_MPC(base.BaseMPC):
         params["critic_params"] = self.critic_model.init(*critic_args)
         return params
 
-    def critic_loss(self, xseq, label, params):
+    def get_grad_penalty(self, true_xseq, pred_xseq, params, key):
         critic_params = params["critic_params"]
-        logit = self.critic_model.predict(xseq, critic_params)
-        ce_loss = optax.sigmoid_binary_cross_entropy(logit, label)
-        ent_loss = -get_logit_bernoulli_entropy(logit)
+
+        ep = jax.random.uniform(key)
+        mix_xseq = ep * true_xseq + (1 - ep) * pred_xseq
+
+        def fun(batch_xseq):
+            logits = jax.vmap(self.critic_model.predict, in_axes=(0, None))(
+                batch_xseq, critic_params
+            )
+            return logits
+
+        mix_logits, fvjp = jax.vjp(fun, batch_mix_xseq)
+
+    def critic_loss(self, true_xseq, pred_xseq, params):
+        critic_params = params["critic_params"]
+
+        true_logit = self.critic_model.predict(true_xseq, critic_params)
+        pred_logit = self.critic_model.predict(pred_xseq, critic_params)
+        true_loss = optax.sigmoid_binary_cross_entropy(
+            true_logit, jnp.ones(true_logit.shape)
+        )
+        pred_loss = optax.sigmoid_binary_cross_entropy(
+            pred_logit, jnp.zeros(pred_logit.shape)
+        )
+        ce_loss = (true_loss + pred_loss) / 2
+
+        true_ent_loss = -get_logit_bernoulli_entropy(true_logit)
+        pred_ent_loss = -get_logit_bernoulli_entropy(pred_logit)
+        ent_loss = (true_ent_loss + pred_ent_loss) / 2
+
         return ce_loss + self.entropy_coef * ent_loss
 
     @functools.partial(jax.jit, static_argnums=0)
-    def critic_loss_and_grad(self, batch_xseq, batch_label, params):
+    def critic_loss_and_grad(
+        self, batch_true_xseq, batch_pred_xseq, params, key
+    ):
         @jax.jit
         def loss_fn(params):
             losses = jax.vmap(self.critic_loss, in_axes=(0, 0, None))(
-                batch_xseq, batch_label, params
+                batch_true_xseq, batch_pred_xseq, params
             )
-            return jnp.mean(losses)
+            grad_penalty = self.get_grad_penalty(
+                batch_true_xseq, batch_pred_xseq, params, key
+            )
+            return jnp.mean(losses) + self.grad_penalty_coef * grad_penalty
 
         loss, grads = jax.value_and_grad(loss_fn)(params)
         return loss, grads
